ec5953257a337a751db7243832dc1a17bcf3c52f
beard58 <87026815+beard58@users.noreply.github.com>
2021-07-30 20:21:27 +0530

    Create triangle_valid.cpp

---
+++ triangle_valid.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c,s;
+
+cout<<"enter the angles";
+cin>>a>>b>>c;
+s=a+b+c;
+if(s==180)
+cout << "the traingel is valid" ;
+else
+cout<<"the triangel is not valid";
+}
