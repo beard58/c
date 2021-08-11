a60392822da68179ff182be0a26c24bf95d4a9c7
beard58 <87026815+beard58@users.noreply.github.com>
2021-08-12 00:06:15 +0530

    Create div.java

---
+++ div.java
@@ -0,0 +1,20 @@
+package test1;
+import java.util.Scanner;
+
+//import com.sun.tools.jdeprscan.scan.Scan;
+//import com.sun.java_cup.internal.runtime.Scanner;
+
+public class data {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter any two numbers");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a/b;
+		System.out.println("addtion of two numbers "+c);
+
+	}
+
+}
